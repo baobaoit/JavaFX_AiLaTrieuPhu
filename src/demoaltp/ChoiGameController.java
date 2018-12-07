05380b8bf39246cecc15e7d80f79e5b48f9ec4f8
BaoLHQ <quocbao.lehoang@gmail.com>
2018-12-07 14:54:58 +0700

    Them xu ly tro giup 50:50

--- src/demoaltp/ChoiGameController.java
+++ src/demoaltp/ChoiGameController.java
@@ -3,15 +3,22 @@ package demoaltp;
 import demoaltp.database.HibernateUtil;
 import demoaltp.modal.CauHoi;
 import demoaltp.modal.MucDo;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -20,6 +27,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -55,6 +63,8 @@ public class ChoiGameController implements Initializable {
     private Button btnChoiceC;
     @FXML
     private Button btnChoiceD;
+    @FXML
+    private Button btnTroGiup5050;
 
     /**
      * Initializes the controller class.
@@ -72,10 +82,19 @@ public class ChoiGameController implements Initializable {
         this.newGame();
     }
 
+    private void resetButtonDisable() {
+        this.btnChoiceA.setDisable(false);
+        this.btnChoiceB.setDisable(false);
+        this.btnChoiceC.setDisable(false);
+        this.btnChoiceD.setDisable(false);
+    }
+
     private void newGame() {
         this.viTriCauHoi = 0;
         this.mocCauHoi = 0;
         this.mucDoCauHoi = 0;
+        this.resetButtonDisable();
+        this.btnTroGiup5050.setDisable(false);
         if (lstCauHoiDe != null) {
             lstCauHoiDe.clear();
         }
@@ -93,26 +112,90 @@ public class ChoiGameController implements Initializable {
     }
 
     @FXML
+    private void troGiup5050(ActionEvent event) {
+        List<String> dapAn = new ArrayList<>();
+        dapAn.add("A");
+        dapAn.add("B");
+        dapAn.add("C");
+        dapAn.add("D");
+        dapAn.remove(curCauHoi.getDapAn());
+        dapAn.remove(dapAn.get((new Random()).nextInt(3))); // Dap an chon con lai
+
+        if (btnChoiceA.getText().substring(0, 1).equals(dapAn.get(0))) {
+            btnChoiceA.setDisable(true);
+        }
+        if (btnChoiceB.getText().substring(0, 1).equals(dapAn.get(0)) || btnChoiceB.getText().substring(0, 1).equals(dapAn.get(1))) {
+            btnChoiceB.setDisable(true);
+        }
+        if (btnChoiceC.getText().substring(0, 1).equals(dapAn.get(0)) || btnChoiceC.getText().substring(0, 1).equals(dapAn.get(1))) {
+            btnChoiceC.setDisable(true);
+        }
+        if (btnChoiceD.getText().substring(0, 1).equals(dapAn.get(1))) {
+            btnChoiceD.setDisable(true);
+        }
+
+        this.btnTroGiup5050.setDisable(true);
+    }
+
+    @FXML
     private void chonDapAnHandler(ActionEvent event) {
         Button btnClicked = (Button) event.getSource();
         String answer = btnClicked.getText().substring(0, 1);
 
-        if (answer.equals(curCauHoi.getDapAn())) {
-            // Tra loi dung
-            if (mocCauHoi < 14) {
-                // Chua toi cau hoi so 15
-                this.viTriCauHoi++;
-                this.mocCauHoi++;
-                this.showCauHoi();
-                this.showMocHienTai();
-                this.resetMocTruoc();
+        btnClicked.setStyle("-fx-background-color: red; -fx-font-weight: BOLD; -fx-font-size: 20");
+
+        msg.setAlertType(Alert.AlertType.CONFIRMATION);
+        msg.setHeaderText("Lựa chọn đáp án " + btnClicked.getText());
+        msg.setContentText("Bạn có chắc đây là đáp án cuối cùng chứ?");
+
+        Optional<ButtonType> option = msg.showAndWait();
+
+        if (option.get() == ButtonType.OK) {
+            if (answer.equals(curCauHoi.getDapAn())) {
+                // Tra loi dung
+                if (mocCauHoi < 14) {
+                    // Chua toi cau hoi so 15
+                    msg.setAlertType(Alert.AlertType.CONFIRMATION);
+                    msg.setHeaderText("Chúc mừng bạn đã trả lời đúng");
+                    msg.setContentText("Bạn muốn tiếp tục (OK) hay ngừng lại (Cancel)?");
+
+                    Optional<ButtonType> optChoiHayDung = msg.showAndWait();
+
+                    if (optChoiHayDung.get() == ButtonType.OK) {
+                        this.viTriCauHoi++;
+                        this.mocCauHoi++;
+                        this.showCauHoi();
+                        this.showMocHienTai();
+                        this.resetMocTruoc();
+                        btnClicked.setStyle("");
+                        this.resetButtonDisable();
+                    } else {
+                        msg.setAlertType(Alert.AlertType.INFORMATION);
+                        msg.setHeaderText("Chúc mừng bạn");
+                        msg.setContentText("Cảm ơn bạn đã tham gia cùng chúng tôi");
+
+                        Optional<ButtonType> optThoat = msg.showAndWait();
+                        if (optThoat.get() == ButtonType.OK) {
+                            try {
+                                Stage curWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+                                curWindow.setScene(new Scene(FXMLLoader.load(getClass().getResource("FXMLDocument_DHGB.fxml"))));
+                            } catch (IOException ex) {
+                                Logger.getLogger(DangNhapController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        } // Xu ly khi OK o thong bao thoat game
+                    } // Xu ly khi hien thong bao dung
+                } else {
+                    // Toi cau hoi so 15
+                    this.showDialogThongBao("Bạn đã chiến thắng", "Chúc mừng bạn đã xuất sắc vượt qua 15 câu hỏi.\nBạn có muốn chơi lại không?");
+                }
             } else {
-                // Toi cau hoi so 15
-                this.showDialogThongBao("Bạn đã chiến thắng", "Chúc mừng bạn đã xuất sắc vượt qua 15 câu hỏi.\nBạn có muốn chơi lại không?");
+                // Tra loi sai
+                this.showDialogThongBao("Bạn đã thua", "Bạn có muốn chơi lại không?");
             }
         } else {
-            // Tra loi sai
-            this.showDialogThongBao("Bạn đã thua", "Bạn có muốn chơi lại không?");
+            // Cancel
+            btnClicked.setStyle("");
         }
     }
 
@@ -208,7 +291,7 @@ public class ChoiGameController implements Initializable {
         this.lstCauHoiKho = cr.list();
         session.close();
     }
-    
+
     private void showMocDiem() {
         for (int i = lstMocDiem.size() - 1; i >= 0; i--) {
             vbCotPhai.getChildren().add(lstMocDiem.get(i));
